d0a7f3c2da442a41abc1c3e6684edb3a9cd5de98
Aleksey Dmitrenko <DIESELOKJ@gmail.com>
2016-02-19 13:14:31 +0200

    Aside complete (not responsive yet)

--- index.php
+++ index.php
@@ -31,7 +31,30 @@
     </div>
 </header>
 
+<div class="container">
+    <aside class="right-aside">
+        <section class="main-descript">
+            <h2>About Blogin.</h2>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <p>Excepteur sint occaecat sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </section>
 
+        <section class="search">
+            <h2>Search.</h2>
+            <form method="post" action="#">
+                <input type="search" name="search" placeholder="What are you looking for?">
+            </form>
+        </section>
+
+        <section class="tuned-mail">
+            <h2>Stay Tuned.</h2>
+            <form method="post" action="#">
+                <input type="email" name="email" placeholder="Your Email">
+                <button type="button" name="send-button"><span class="fa fa-arrow-circle-o-right"></span></button>
+            </form>
+        </section>
+    </aside>
+</div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="js/script-menu.js"></script>
