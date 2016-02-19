e0980cfcb9d10bc9c9e044ecbc01e07ab5ab772b
Aleksey Dmitrenko <DIESELOKJ@gmail.com>
2016-02-19 17:21:23 +0200

    Responsive content

--- index.php
+++ index.php
@@ -33,28 +33,57 @@
 
 <div class="container">
     <aside class="right-aside">
-        <section class="main-descript">
-            <h2>About Blogin.</h2>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <p>Excepteur sint occaecat sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </section>
+            <section class="main-descript">
+                <h2>About Blogin.</h2>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p>Excepteur sint occaecat sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
 
-        <section class="search">
-            <h2>Search.</h2>
-            <form method="post" action="#">
-                <input type="search" name="search" placeholder="What are you looking for?">
-            </form>
-        </section>
+            <section class="search">
+                <h2>Search.</h2>
+                <form method="post" action="#">
+                    <input type="search" name="search" placeholder="What are you looking for?">
+                </form>
+            </section>
 
-        <section class="tuned-mail">
-            <h2>Stay Tuned.</h2>
-            <form method="post" action="#">
-                <input type="email" name="email" placeholder="Your Email">
-                <button type="button" name="send-button"><span class="fa fa-arrow-circle-o-right"></span></button>
-            </form>
-        </section>
+            <section class="tuned-mail">
+                <h2>Stay Tuned.</h2>
+                <form method="post" action="#">
+                    <input type="email" name="email" placeholder="Your Email">
+                    <button type="button" name="send-button"><span class="fa fa-arrow-circle-o-right"></span></button>
+                </form>
+            </section>
     </aside>
+
+    <main class="content">
+            <article>
+                <img src="images/article-img-0.jpg" alt="Duis aute irure dolor in henderit in voluptate.">
+                <h2>Duis aute irure dolor in henderit in voluptate.</h2>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipop ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
+                <a href="#">Continue Reading</a>
+            </article>
+
+            <article>
+                <img src="images/article-img-01.jpg" alt="Duis aute irure dolor in henderit in voluptate.">
+                <h2>Duis aute irure dolor in henderit in voluptate.</h2>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipop ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
+                <a href="#">Continue Reading</a>
+            </article>
+    </main>
 </div>
+
+    <section class="navigation">
+        <div class="container">
+            <ul>
+                <li><a href="#"><span class="fa fa-long-arrow-left"></span>Previous</a></li>
+                <li><a href="#">Next<span class="fa fa-long-arrow-right"></span></a></li>
+            </ul>
+        </div>
+    </section>
+
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="js/script-menu.js"></script>
